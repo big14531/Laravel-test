f7c09b37f5c613c73a546b36153d35522d0872e4
Yutchaii <yuthapichai14531@gmail.com>
2018-02-12 12:23:04 +0700

    create image model , controller , view

---
+++ resources/views/images.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <h1>Gallery</h1>
+    <hr>
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+
+
+        </div>
+    </div>
+</div>
+@endsection
